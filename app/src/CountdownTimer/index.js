52b2a717b0fdc4875ff1bb60e02b093d2d4c72c2
Kyle Gilliam <kgilliam125@gmail.com>
2022-02-18 15:37:08 -0800

    Copy/CSS tweaks; Background image

--- app/src/CountdownTimer/index.js
+++ app/src/CountdownTimer/index.js
@@ -36,7 +36,7 @@ const CountdownTimer = ({ dropDate }) => {
 
 	return (
 		<div className="timer-container">
-		<p className="timer-header">Candy Drop Starting In</p>
+		<p className="timer-header">Daisy arrives in...</p>
 		{timerString && <p className="timer-value">{`⏰ ${timerString}`}</p>}
 		</div>
 	)
